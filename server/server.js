d9e249405564a637c082bf35fde3a946a09c1869
Charles Reyes <charleskylereyes16@gmail.com>
2023-03-16 23:39:48 +0800

    Added cors

--- server/server.js
+++ server/server.js
@@ -1,5 +1,8 @@
 const app = require("./app");
+const cors = require( "cors" );
 
 const port = process.env.PORT || 3000;
+
+app.use( cors( { origin: `http://localhost:${ port }` } ) );
 
 app.listen(port, () => console.log(`Server running on port ${port}`));
