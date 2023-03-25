938a4f4e9d6661cd8bb64d428319c8a11943a148
Charles Reyes <charleskylereyes16@gmail.com>
2023-03-25 21:03:48 +0800

    Added new rubrics migration and model

---
+++ server/app/models/rubric.js
@@ -0,0 +1,33 @@
+"use strict";
+const { Model } = require("sequelize");
+module.exports = (sequelize, DataTypes) => {
+  class rubric extends Model {
+    /**
+     * Helper method for defining associations.
+     * This method is not a part of Sequelize lifecycle.
+     * The `models/index` file will call this method automatically.
+     */
+    static associate(models) {
+      // define association here
+    }
+  }
+  rubric.init(
+    {
+      description: DataTypes.STRING,
+      rate1: DataTypes.STRING,
+      rate2: DataTypes.STRING,
+      rate3: DataTypes.STRING,
+      rate4: DataTypes.STRING,
+      rate5: DataTypes.STRING,
+      weight: DataTypes.DOUBLE,
+      rubric_type: DataTypes.STRING,
+      pbl_level: DataTypes.STRING,
+      category: DataTypes.STRING,
+    },
+    {
+      sequelize,
+      modelName: "rubric",
+    }
+  );
+  return rubric;
+};
