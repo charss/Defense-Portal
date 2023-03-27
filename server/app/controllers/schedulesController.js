c6c43e61ee4b8f6c8ca5e504ca823fb2c93b4b5f
Charles Reyes <charleskylereyes16@gmail.com>
2023-03-27 21:52:26 +0800

    Updated formatting

--- server/app/controllers/schedulesController.js
+++ server/app/controllers/schedulesController.js
@@ -1,29 +1,29 @@
-const schedules = require("../models").schedules;
-const thesis_defenses = require("../models").thesis_defenses;
-const title_defenses = require("../models").title_defenses;
+const Schedules = require("../models").schedules;
+const ThesisDefenses = require("../models").thesis_defenses;
+const TitleDefenses = require("../models").title_defenses;
 
-exports.index = (req, res) => {
+exports.getAll = (req, res) => {
   // Get all schedules
-  return schedules
-    .findAll({ include: [thesis_defenses, title_defenses] })
-    .then((data) => {
+  return Schedules.findAll({ include: [ThesisDefenses, TitleDefenses] }).then(
+    (data) => {
       if (!data) {
-        res.status(404).send({ error: "Schedule not found" });
+        res.status(404).send({ error: "No schedule found" });
       } else {
         res.status(200).send(data);
       }
-    });
+    }
+  );
 };
 
-exports.show = async (req, res) => {
+exports.getById = async (req, res) => {
   // Get schedule by ID
-  return schedules
-    .findByPk(req.params.id, { include: [thesis_defenses, title_defenses] })
-    .then((schedule) => {
-      if (!schedule) {
-        res.status(404).send({ error: "Schedule not found" });
-      } else {
-        res.status(200).send(schedule);
-      }
-    });
+  return Schedules.findByPk(req.params.id, {
+    include: [ThesisDefenses, TitleDefenses],
+  }).then((schedule) => {
+    if (!schedule) {
+      res.status(404).send({ error: "Schedule not found" });
+    } else {
+      res.status(200).send(schedule);
+    }
+  });
 };
