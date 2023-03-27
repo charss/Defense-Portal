0817b403701a1225e7f559dbd4f5cdaba3e549de
Charles Reyes <charleskylereyes16@gmail.com>
2023-03-27 19:03:33 +0800

    Added basic CRUD for panelists (w/o create & delete)

--- server/app/routes/panelistsRoutes.js
+++ server/app/routes/panelistsRoutes.js
@@ -4,4 +4,5 @@ module.exports = (app) => {
   app.get("/panelists", panelistsController.getAll);
   app.get("/panelists/test", panelistsController.test);
   app.get("/panelists/:id", panelistsController.getById);
+  app.put("/panelists/:id", panelistsController.update);
 };
