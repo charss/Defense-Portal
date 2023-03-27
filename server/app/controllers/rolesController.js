757cee2eb88a17b15c2c7194ee3ea6d8c1f70c5e
Charles Reyes <charleskylereyes16@gmail.com>
2023-03-27 21:17:16 +0800

    Added get all and by id for roles URI

--- server/app/controllers/rolesController.js
+++ server/app/controllers/rolesController.js
@@ -1,17 +1,29 @@
-const roles = require("../models").roles;
-const panelists = require("../models").panelists;
+const Roles = require("../models").roles;
+const Panelists = require("../models").panelists;
 
 const roleOptions = {
   include: [
     {
-      model: panelists,
+      model: Panelists,
       as: "panelists",
     },
   ],
 };
 
-exports.show = (req, res) => {
-  return roles.findByPk(req.params.id, roleOptions).then((role) => {
+exports.getAll = (req, res) => {
+  // Get all roles
+  return Roles.findAll().then((roles) => {
+    if (!roles) {
+      res.status(404).send({ error: "Roles not found" });
+    } else {
+      res.status(200).send(roles);
+    }
+  });
+};
+
+exports.getById = (req, res) => {
+  // Get roles by ID
+  return Roles.findByPk(req.params.id).then((role) => {
     if (!role) {
       res.status(404).send({ error: "Role not found" });
     } else {
