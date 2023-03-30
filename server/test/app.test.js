e7c5cfb66fb93c6a9f318d52e4fbec9ca9498d08
Charles Reyes <charleskylereyes16@gmail.com>
2023-03-30 21:46:08 +0800

    Deleted file

--- server/test/app.test.js
+++
@@ -1,8 +0,0 @@
-const request = require("supertest");
-
-const app = require("../app");
-
-test("Environment File should be .env.test", () => {
-  expect(process.env.TRY).toBe("TEST ENV FILE");
-});
-
